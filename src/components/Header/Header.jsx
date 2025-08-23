da61477a74bdc943e92c88cf988dcc1fdd1de66e
Yuliia Fomenko <yaulia3@gmail.com>
2025-08-23 22:11:28 +0100

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import sprite from '../../assets/sprite.svg'
+import { Link, NavLink } from 'react-router-dom'
+import s from './Header.module.css'
+import clsx from 'clsx'
 
 const Header = () => {
+  const activeStyle = ({ isActive}) => {
+    return clsx(s.link, isActive && s.active)
+  }
+
   return (
-    <div>Header</div>
-  )
+    <div className={s.header}>
+      <Link to="/">
+        <svg width="136" height="15">
+          <use href={`${sprite}#icon-Logo`} />
+        </svg>
+      </Link>
+      <nav className={s.nav}>
+        <NavLink to="/" className={activeStyle}>
+          Home
+        </NavLink>
+        <NavLink to="/catalog" className={activeStyle}>
+          Catalog
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default Header
